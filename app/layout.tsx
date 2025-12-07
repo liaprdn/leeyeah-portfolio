731823881c12b9778b7e1261bdd7a3dc833b2d10
Aulia Pradina <liaprdn@192.168.1.7>
2025-12-07 13:32:35 +0700

    fix the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Leeyeah Portfolio",
+  title: "Aulia Pradina Portfolio",
   description: "Personal Portfolio created with Next.js",
 };
 
